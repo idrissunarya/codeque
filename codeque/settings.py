47fa30802e4273e0c9df2350ee53c659a936f0d7
idris sunarya <idris.sunarya@buzz.co.id>
2020-12-19 10:57:13 +0700

    update

--- codeque/settings.py
+++ codeque/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'codeque.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = '/Users/idris/vps/codeque/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
