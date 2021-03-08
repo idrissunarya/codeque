55c74cb3bb17cc68eaf77d59994d0de230c43697
idris sunarya <idris.sunarya@buzz.co.id>
2021-03-08 09:18:34 +0700

    update all

--- apps/web/views.py
+++ apps/web/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'web/index.html')
+
+def signup(request):
+    return render(request, 'web/pages/signup.html')
+
+def login(request):
+    return render(request, 'web/pages/login.html')
